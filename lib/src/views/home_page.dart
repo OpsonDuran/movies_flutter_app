15d23e0220542d7efa26d997460216901c87eae2
OpsonDuran <iccarmandoduran@gmail.com>
2019-06-29 00:01:06 -0500

    move card to widget custom and add mediaquery for adap in mobiles Screens

--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:movies_flutter_app/src/widgets/card_swiper_widget.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -32,19 +33,8 @@ class HomePage extends StatelessWidget {
 
 
   Widget _swiperTargets(){
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        layout: SwiperLayout.STACK ,
-        itemWidth: 200.0,
-          itemBuilder: (BuildContext context,int index){
-            return new Image.network("http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,);
-          },
-          itemCount: 3,
-        ),
+    return CardSwiper(
+      movies: [1,2,3,4,5],
     );
   }
 }
